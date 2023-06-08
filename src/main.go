d10ab98288a8d9011b17caef4212814b494e969a
steven vargas <stevenvargasw380@gmail.com>
2023-06-08 14:14:24 -0500

    Variables

--- src/main.go
+++ src/main.go
@@ -1,8 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Print("Hola Mundo")
+
+	//Declaracion de constatntes
+	const pi float64 = 3.14
+	const pi2 = 3.1415
+
+	fmt.Println("pi:", pi)
+	fmt.Println("pi2:", pi2)
+
+	//Declaracion de variables enteras
+	base := 12
+	var altura int = 14
+	var area int
+
+	fmt.Println(base, altura, area)
+
+	//Zero values
+	var a int     // enteros
+	var b float64 // decimales
+	var c string  //
+	var d bool    // true o false
+
+	fmt.Println(a, b, c, d)
+
+	//Area cuadrado
+	const baseCuadrado = 10
+	areaCuadrado := baseCuadrado * baseCuadrado
+	fmt.Println("Area Cuadrado: ", areaCuadrado)
+
+	x := 10
+	y := 50
+	//Suma
+	result := x + y
+	fmt.Println("Suma: ", result)
+
+	//Resta
+	result = y - x
+	fmt.Println("Resta: ", result)
+	//Multiplicacion
+	result = x + y
+	fmt.Println("Multiplicacion: ", result)
+
+	//Division
+	result = y / x
+	fmt.Println("Division: ", result)
+
+	//Modulo
+	result = y % x
+	fmt.Println("Modulo: ", result)
+	//incremental
+	x++
+	fmt.Println("Incremental: ", x)
 
 }
